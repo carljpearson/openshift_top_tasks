a4a62d867bc1b6c45f5e6b862ede0f00c2489f74
carljpearson <carl.j.pearson@gmail.com>
2020-02-11 10:26:09 -0600

    added difficulty charts

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 
-data <- read_csv("/Users/carlpearson/Documents/r_github/openshift_top_tasks/do_not_upload/initial_eight.csv",col_names = T)
+data <- read_csv("/Users/carlpearson/Documents/r_github/openshift_top_tasks/do_not_upload/three_weeks.csv",col_names = T)
 
 df <- data[3:nrow(data),]
 
 #get wide df for level 1 vars
 df.wide <-  df %>% 
-  filter(Status != "Survey Preview") %>% #remove previews
+  filter(Status != "Survey Preview",#remove previews
+         RH=="Yes, I'm a Red Hat employee") %>%  #remove internals
   select(ResponseId, #id variable
          rh=RH, #internal/external variablke
          experience=Experience, #xperience level
@@ -23,6 +24,14 @@ df.wide <-  df %>%
          contains("ui_topt"),
          contains("ui_rank"),
          contains("ui_diff"),
+         progress=Progress,
+         duration=`Duration (in seconds)`,
+         data=RecordedDate,
+         channel=DistributionChannel,
+         lat=LocationLatitude,
+         long=LocationLongitude,
+         version,
+         research
   ) %>%
   select(-contains("DO_")) %>% #remove randomization data
   unite(col="role",contains("role"),na.rm=T,sep=",") %>% #combine role cols
@@ -65,41 +74,9 @@ df.wide <-  df %>%
 
 
 #get long df for level 2 vars
-df.long <- df %>% 
-  filter(Status != "Survey Preview") %>% #remove previews
-  select(ResponseId, #id variable
-         rh=RH, #internal/external variablke
-         experience=Experience, #xperience level
-         contains("role"), #role
-         #UI/CLI
-         use_oc=use_interface_1,
-         use_odo=use_interface_2,
-         use_ui=use_interface_3,
-         umux.eff = Q7_1,
-         umux.eas = Q7_2,
-         contains("cli_topt"),
-         contains("cli_rank"),
-         contains("cli_diff"),
-         contains("ui_topt"),
-         contains("ui_rank"),
-         contains("ui_diff"),
-         ) %>%
-  select(-contains("DO_")) %>% #remove randomization data
-  unite(col="role",contains("role"),na.rm=T,sep=",") %>% #combine role cols
-  mutate(role=gsub(",,","",role)) %>%
-  pivot_longer(contains("i_",),names_to = "variable",values_to = "response" ) %>% #elongate top task measurement variables
-  na.omit() %>% #remove na, unpicked choices
-  separate(variable,into=c("interface","measure","number"),sep="_") %>% #split out variable col into three pieces of info
-  unite("variable",c("interface","measure"),sep="_") %>%
-  select(-number) %>% #remove number variable, unneeded
-  pivot_wider(names_from = "variable",values_from = "response") %>% #widen data across ui/cli measure
+df.long <- df.wide %>% 
   unnest() %>%
-  separate(experience, into = c('experience','drop_me'),sep=" – ") %>% select(-drop_me) %>%
-  mutate(
-    experience = factor(experience, levels = c("None","Basic","Intermediate","Advanced","Expert")),
-    use_oc = factor(use_oc,levels=c("I'm not sure","Never","Less than once","1-2 times", "3-5 times", "6-10 times","11+ times")),
-    use_odo = factor(use_odo,levels=c("I'm not sure","Never","Less than once","1-2 times", "3-5 times", "6-10 times","11+ times")),
-    use_ui = factor(use_ui,levels=c("I'm not sure","Never","Less than once","1-2 times", "3-5 times", "6-10 times","11+ times")),
+  mutate( #numeric rank and difficulty
     cli_diff.n = case_when(cli_diff == "Very Easy" ~ 5,
                            cli_diff == "Easy" ~ 4,
                            cli_diff == "Neutral" ~ 3,
@@ -113,3 +90,4 @@ df.long <- df %>%
                            ui_diff == "Very Difficult" ~ 1
     )
   )
+
--- plotting.R
+++ plotting.R
@@ -1,11 +1,30 @@
-#Creat Top Task plots
+#level 1 plots: descriptives
+
+#Experience
+df.wide %>%
+  ggplot(aes(experience,fill=experience)) +
+  geom_bar() +
+  geom_text(stat = 'count',aes(label =..count.., vjust = -0.4),size=7) + 
+  ggthemes::theme_tufte(base_family="sans", base_size = 15) +
+  scale_fill_brewer(palette = "YlOrRd") +
+  theme(
+    axis.text.x= element_text(size=12),
+    axis.text.y=element_blank(),
+    axis.title.x =element_blank(),
+    axis.ticks.y=element_blank(),
+    legend.position = "none") +
+  labs(y="Count")
+
+ggsave("/Users/carlpearson/Documents/r_github/openshift_top_tasks/plots/experience.png",bg="transparent",width = 8,height = 6)
+
+
+#Create Top Task plots
 
 #static values
 total.responses <- length(unique(df$ResponseId))
 zval=1.64
 
 #proportions dataframe
-
 #cli
 df.sum.cli <- df.long %>%
   mutate(total=total.responses,
@@ -21,6 +40,8 @@ df.sum.cli <- df.long %>%
   summarize(count=n(),
             rank_avg=mean(as.numeric(cli_rank),na.rm = T),
             difficulty_avg=mean(cli_diff.n,na.rm = T),
+            difficulty_sd = sd(cli_diff.n, na.rm=T ),
+            difficulty_marg = ((difficulty_sd/sqrt(count))*zval),
             expertise_avg = mean(expertise.num,na.rm = T)          
   ) %>%
   mutate(prop = count / total, #get cis
@@ -36,12 +57,16 @@ df.sum.cli <- df.long %>%
          upperci = ifelse(upperci >= 1, 1, upperci)) #keep upper ci below 1
 
 #ui
+
+#get summarized dataset
 df.sum.ui <- df.long %>%
   mutate(total=total.responses) %>%
   group_by(ui_topt,total) %>%
   summarize(count=n(),
             rank_avg=mean(as.numeric(ui_rank),na.rm = T),
             difficulty_avg=mean(ui_diff.n,na.rm = T),
+            difficulty_sd = sd(ui_diff.n, na.rm=T ),
+            difficulty_marg = ((difficulty_sd/sqrt(count))*zval),
             expertise_avg = mean(expertise.num,na.rm = T)          
   ) %>%
   mutate(prop = count / total, #get cis
@@ -98,3 +123,102 @@ df.sum.ui %>%
     x="Tasks",
     y="Percentage"
   )
+
+#difficulty side by side
+
+#cli
+p1 <- df.sum.cli %>% 
+  filter(prop>.02) %>%
+  ggplot(aes(y=prop,x=reorder(cli_topt,prop))) +
+  geom_bar(stat = "identity") +
+  # geom_point(aes(y=difficulty_avg)) +
+  geom_errorbar(aes(ymin=lowerci,ymax=upperci),color="gray",width=.5) +
+  geom_errorbar(aes(ymin=lowerci,ymax=upperci),color="gray",width=.5) +
+  scale_y_reverse(labels = scales::percent) +
+  theme_minimal() +
+  coord_flip() +
+  #theme
+  #theme(axis.text.x = element_text(angle = -45,hjust=.7,vjust=1))
+  # ggthemes::theme_tufte(base_family="sans") +
+  labs(
+    title="RHEL Top 20 Tasks - Proprotion Chosen",
+    subtitle = "Confidence internals at 90%, Adjusted Wald method",
+    x="Tasks",
+    y="Percentage"
+  )
+
+p2 <- df.sum.cli %>% 
+  filter(prop>.02) %>%
+  ggplot(aes(y=difficulty_avg,x=reorder(cli_topt,prop))) +
+  geom_bar(stat = "identity") +
+  geom_errorbar(aes(ymin=difficulty_avg-difficulty_marg,ymax=difficulty_avg+difficulty_marg),color="gray",width=.5) +
+
+  coord_flip() +
+  theme_minimal() +
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position = "none") +
+  # ggthemes::theme_tufte(base_family="sans") +
+  labs(
+    title="RHEL Top 20 Tasks - Average Difficulty",
+    subtitle = "Confidence internals at 90%, standard method",
+    x="Tasks",
+    y="Dfficulty rating"
+  )
+
+
+gridExtra::grid.arrange(p1, p2, nrow = 1) 
+
+
+#save
+#ggsave("/Users/carlpearson/Documents/r_github/top_tasks_rhel/difficulty.png",device="png",  width=8,height=6,bg="white")
+
+
+#ui
+p1 <- df.sum.ui %>% 
+  filter(prop>.02) %>%
+  ggplot(aes(y=prop,x=reorder(ui_topt,prop))) +
+  geom_bar(stat = "identity") +
+  # geom_point(aes(y=difficulty_avg)) +
+  geom_errorbar(aes(ymin=lowerci,ymax=upperci),color="gray",width=.5) +
+  geom_errorbar(aes(ymin=lowerci,ymax=upperci),color="gray",width=.5) +
+  scale_y_reverse(labels = scales::percent) +
+  theme_minimal() +
+  coord_flip() +
+  #theme
+  #theme(axis.text.x = element_text(angle = -45,hjust=.7,vjust=1))
+  # ggthemes::theme_tufte(base_family="sans") +
+  labs(
+    title="RHEL Top 20 Tasks - Proprotion Chosen",
+    subtitle = "Confidence internals at 90%, Adjusted Wald method",
+    x="Tasks",
+    y="Percentage"
+  )
+
+p2 <- df.sum.ui %>% 
+  filter(prop>.02) %>%
+  ggplot(aes(y=difficulty_avg,x=reorder(ui_topt,prop))) +
+  geom_bar(stat = "identity") +
+  geom_errorbar(aes(ymin=difficulty_avg-difficulty_marg,ymax=difficulty_avg+difficulty_marg),color="gray",width=.5) +
+  
+  coord_flip() +
+  theme_minimal() +
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position = "none") +
+  # ggthemes::theme_tufte(base_family="sans") +
+  labs(
+    title="",
+    subtitle = "",
+    x="Tasks",
+    y="Dfficulty rating"
+  )
+
+
+gridExtra::grid.arrange(p1, p2, nrow = 1) 
+
+
+#save
+#ggsave("/Users/carlpearson/Documents/r_github/top_tasks_rhel/difficulty.png",device="png",  width=8,height=6,bg="white")
